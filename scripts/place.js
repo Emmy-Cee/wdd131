84cce587da6d2af280eeba6b747a19dda271abd2
Emmy-Cee <emmyatatcee33@gmail.com>
2026-07-18 15:01:32 +0100

    Error from feedback

--- scripts/place.js
+++ scripts/place.js
@@ -2,7 +2,6 @@ const currentYear = document.getElementById("currentYear");
 
 currentYear.innerHTML = new Date().getFullYear();
 
-
 const lastModified = document.getElementById("lastModified");
 
 lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat("en-US",
@@ -10,3 +9,22 @@ lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.Date
 		dateStyle: "full"
 	}
     ).format(new Date(document.lastModified))}</span>`;
+
+const windChill = document.getElementById("windChill");
+const windChillValue = windChill.querySelector(".value");
+
+const tempElement = document.querySelector(".temp .value");
+const windElement = document.querySelector(".wind .value");
+
+const temperature = parseFloat(tempElement.textContent);
+const windSpeed = parseFloat(windElement.textContent);
+
+function calculateWindChill(temperature, windSpeed) {
+    return 35.74 + (0.6215 * temperature) - 35.75 * (windSpeed ** 0.16) + (0.4275 * temperature * (windSpeed ** 0.16));
+}
+
+if (temperature <= 50 && windSpeed > 3) {
+    windChillValue.textContent = `${calculateWindChill(temperature, windSpeed).toFixed(1)}°F`;
+} else {
+    windChillValue.textContent = "N/A";
+}

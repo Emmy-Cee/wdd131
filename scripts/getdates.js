d9a9f7040aab9ac2ef8cd61488834fdf0939fc9e
Emmy-Cee <emmyatatcee33@gmail.com>
2026-09-08 21:47:31 +0100

    Add favicon and update HTML head section

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,5 @@
 const currentYear = document.getElementById("currentyear");
-
 currentYear.innerHTML = new Date().getFullYear();
-
-
 const lastModified = document.getElementById("lastModified");
 
-lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat("en-US",
-	{
-		dateStyle: "full"
-	}
-    ).format(new Date(document.lastModified))}</span>`;
+lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat("en-US",{dateStyle: "full"}).format(new Date(document.lastModified))}</span>`;
